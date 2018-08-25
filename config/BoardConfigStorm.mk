2c40b039495510044a12e357c7d652bfd5eb60f0
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    stormbreaker: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- config/BoardConfigStorm.mk
+++ config/BoardConfigStorm.mk
@@ -1,5 +1,10 @@
 # Kernel
 include vendor/stormbreaker/config/BoardConfigKernel.mk
 
+# Qcom-specific bits
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/stormbreaker/config/BoardConfigQcom.mk
+endif
+
 # Soong
 include vendor/stormbreaker/config/BoardConfigSoong.mk
