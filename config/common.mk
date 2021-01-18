aosp-star/platform_vendor_starlight

vendor: copy backuptool files to system For some reason in android11 it fails to copy to /install, so copy to system as a temporary workaround till the root cause is discovered
Change-Id: I14f8d38436a955f4fe8f491cfe7be28b58a2984e
Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

config/common.mk
@@ -34,8 +34,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/starlight/products/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/starlight/products/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/starlight/products/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/starlight/products/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/starlight/products/common/bin/50-base.sh:system/addon.d/50-base.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)

StyxProject/vendor_styx

Signed-off-by: ItsVixano <vixanonew@gmail.com>

config/common.mk
@@ -43,8 +43,8 @@ PRODUCT_PACKAGES += \
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/styx/products/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/styx/products/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/styx/products/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/styx/products/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/styx/products/common/bin/50-base.sh:system/addon.d/50-base.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)