aosp-star/platform_vendor_starlight

vendor: Add backuptools
Change-Id: If42aa8b3f97ccded3a9f470e1c7c1b393680a461

config/common.mk
@@ -32,3 +32,15 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/starlight/products/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/starlight/products/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/starlight/products/common/bin/50-base.sh:system/addon.d/50-base.sh
+
+ifneq ($(AB_OTA_PARTITIONS),)
+PRODUCT_COPY_FILES += \
+    vendor/starlight/products/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/starlight/products/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/starlight/products/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif

StyxProject/vendor_styx

Signed-off-by: ItsVixano <vixanonew@gmail.com>

config/common.mk
@@ -40,3 +40,16 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 # ThemePicker
 PRODUCT_PACKAGES += \
     ThemePicker
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/styx/products/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/styx/products/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/styx/products/common/bin/50-base.sh:system/addon.d/50-base.sh
+
+ifneq ($(AB_OTA_PARTITIONS),)
+PRODUCT_COPY_FILES += \
+    vendor/styx/products/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/styx/products/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/styx/products/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif