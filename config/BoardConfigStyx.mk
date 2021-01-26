737c98034f16c11799b398d542d971812e73606a
Gagan Malvi <malvi@aospa.co>
2021-01-26 07:54:15 +0100

    BoardConfig: add namespace for framework detect
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- config/BoardConfigStyx.mk
+++ config/BoardConfigStyx.mk
@@ -6,5 +6,10 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/styx/config/BoardConfigQcom.mk
 endif
 
+# Namespace for fwk-detect
+TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
+PRODUCT_SOONG_NAMESPACES += \
+    $(TARGET_FWK_DETECT_PATH)/fwk-detect
+
 # Soong
 include vendor/styx/config/BoardConfigSoong.mk
