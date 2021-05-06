5f2a46365b6621e3ea0e5dd4505f1bb8773af387
Gagan Malvi <malvi@aospa.co>
2021-05-06 13:02:54 +0530

    styx: Guard QTI FWK
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- config/BoardConfigStyx.mk
+++ config/BoardConfigStyx.mk
@@ -6,10 +6,12 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/styx/config/BoardConfigQcom.mk
 endif
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 # Namespace for fwk-detect
 TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
 PRODUCT_SOONG_NAMESPACES += \
     $(TARGET_FWK_DETECT_PATH)/fwk-detect
+endif
 
 # Soong
 include vendor/styx/config/BoardConfigSoong.mk
