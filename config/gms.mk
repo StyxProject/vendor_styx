578fb93ec36466170e93efa421610cc29e23aa40
althafvly <althafvly@gmail.com>
2021-06-27 09:36:56 +0200

    gms: set TARGET_GAPPS_ARCH
    taking via envsetup is not good

--- config/gms.mk
+++ config/gms.mk
@@ -1,10 +1,13 @@
 ifneq ($(TARGET_NO_GAPPS), true)
 $(warning GAPPS included. Set TARGET_NO_GAPPS to true for skipping GAPPS)
-ifeq ($(TARGET_ARCH),arm64)
+ifeq ($(TARGET_GAPPS_ARCH),)
+$(error Define TARGET_GAPPS_ARCH in your styx_codename.mk file)
+endif
+ifeq ($(TARGET_GAPPS_ARCH),arm64)
 $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
-else ifeq ($(TARGET_ARCH),arm)
+else ifeq ($(TARGET_GAPPS_ARCH),arm)
 $(call inherit-product-if-exists, vendor/gapps/arm/arm-vendor.mk)
-else ifneq ($(filter x86 x86_64,$(TARGET_ARCH)),)
+else ifneq ($(filter x86 x86_64,$(TARGET_GAPPS_ARCH)),)
 $(call inherit-product-if-exists, vendor/gapps/x86/x86-vendor.mk)
 endif
 endif
