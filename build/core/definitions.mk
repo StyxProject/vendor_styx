LineageOS/android_vendor_lineage

build: Reorganize inclusion of QCOM macros
* There's actually not a whole lot QC-specific about these macros,
  make them available at definition.mk inclusion time rather than
  relying on BoardConfig to include them.
* Don't create a dependency on QCOM macros from a makefile
  that is included very early in the build process.

Change-Id: I3dcaf0e068ca44732cfe58f69384621781ddda67

build/core/definitions.mk
@@ -4,3 +4,6 @@
 define uniq
 $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 endef
+
+# Include QCOM macros
+include vendor/lineage/build/core/qcom_utils.mk

StyxProject/vendor_styx

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

build/core/definitions.mk
@@ -1,0 +1,2 @@
+# Include QCOM macros
+include vendor/styx/build/core/qcom_utils.mk