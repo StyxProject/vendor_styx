d48335c98d45d40b739f774f7cc625f00d979b2c
Amy <amy@amyrom.ml>
2021-10-22 19:53:23 +0000

    styx: Add GSI target
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Rashed Abdel-Tawab <rashed@linux.com>
    Co-authored-by: Simon Shields <simon@lineageos.org>

---
+++ build/target/product/styx_arm64.mk
@@ -0,0 +1,30 @@
+# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2021 The Styx Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/aosp_arm64.mk)
+$(call inherit-product, build/target/product/gsi_release.mk)
+
+$(call inherit-product, vendor/styx/config/common.mk)
+
+PRODUCT_COPY_FILES += \
+    device/generic/goldfish/data/etc/permissions/privapp-permissions-goldfish.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-goldfish.xml \
+
+# Allow building otatools
+TARGET_FORCE_OTA_PACKAGE := true
+
+TARGET_PROVIDES_KERNEL_MAKEFILE := true
+TARGET_NO_KERNEL := true
+
+PRODUCT_NAME := styx_arm64
