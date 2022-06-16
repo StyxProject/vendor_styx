520de973bc01ce8da0e3585f5213797bd75c60f9
Gagan Malvi <malvigagan@gmail.com>
2022-06-16 18:44:59 +0200

    gms: refactor to use partner_gms if INTERNAL_FACTORY_BUILD is set.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -31,11 +31,17 @@ $(call inherit-product, vendor/styx/prebuilts/targets.mk)
 # Inherit overlays
 $(call inherit-product, vendor/styx/overlay/overlays.mk)
 
+ifeq ($(INTERNAL_FACTORY_BUILD),true)
+# Include partner GMS target
+$(call inherit-product, vendor/partner_gms/products/gms.mk)
+$(call inherit-product, vendor/partner_gms/products/turbo.mk)
+else
 # Include GMS, Modules, and Pixel features.
 $(call inherit-product, vendor/google/gms/config.mk)
 $(call inherit-product, vendor/google/pixel/config.mk)
+endif
 
-ifeq ($(TARGET_FLATTEN_APEX), false)
+ifeq ($(TARGET_FLATTEN_APEX),false)
 $(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules_s.mk)
 else
 $(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules_s_flatten_apex.mk)
