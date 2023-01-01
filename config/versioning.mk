78bd792e728ca97fecd2e45989701c828c111a07
SahilSonar <sss.sonar2003@gmail.com>
2023-01-01 06:55:02 +0100

    config: Bump version to 3.0.0+staging1

--- config/versioning.mk
+++ config/versioning.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-2019 The Pixel3ROM Project
 # Copyright (C) 2020 Raphielscape LLC.
 # Copyright (C) 2020 Team StormBreaker
-# Copyright (C) 2021 The Styx Project
+# Copyright (C) 2023 The Styx Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,24 +17,24 @@
 # limitations under the License.
 #
 
-PLATFORM_STYX_VERSION_MAJOR := 2
-PLATFORM_STYX_VERSION_MINOR := 3-rc
-PLATFORM_STYX_RELEASE := $(PLATFORM_STYX_VERSION_MAJOR).$(PLATFORM_STYX_VERSION_MINOR)
+PLATFORM_STYX_VERSION := 3.0.0
+PLATFORM_STYX_VERSION_INCR := staging1
+PLATFORM_STYX_VERSION := $(PLATFORM_STYX_VERSION)+$(PLATFORM_STYX_VERSION_INCR)
 
 # Guidelines for setting Styx Build ID:
-#  - SR2  = S Release 2
-#  - 32   = API Level 32
+#  - TR1  = T Release 1
+#  - 33   = API Level 33
 #  - OSR  = Open Source Release
-#  - RF04 = Production Release 04
+#  - RF00 = Production Release 00
 
-PLATFORM_STYX_BUILD_ID := SR2.32.OSR.RF04
+PLATFORM_STYX_BUILD_ID := TR1.33.OSR.RF00
 
-ifeq ($(STYX_BUILD_VARIANT),OFFICIAL)
-    PLATFORM_STYX_PACKAGE += styx-$(PLATFORM_STYX_RELEASE)-$(TARGET_DEVICE)-$(shell date +%m%d%H%M)-OFFICIAL
-else
-    PLATFORM_STYX_PACKAGE += styx-$(PLATFORM_STYX_RELEASE)-$(TARGET_DEVICE)-$(shell date +%m%d%H%M)-UNOFFICIAL
+PLATFORM_STYX_VERSION := $(PLATFORM_STYX_VERSION)-$(TARGET_DEVICE)-$(shell date +%Y%m%d%S)
+
+ifneq ($(STYX_BUILD_VARIANT),)
+    PLATFORM_STYX_VERSION := $(PLATFORM_STYX_VERSION)-$(TARGET_DEVICE)-$(shell date +%Y%m%d%S)-$(STYX_BUILD_VARIANT)
 endif
 
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.system.styx.version=$(PLATFORM_STYX_RELEASE) \
+    ro.styx.version=$(PLATFORM_STYX_VERSION) \
     ro.styx.build.id=$(PLATFORM_STYX_BUILD_ID)
