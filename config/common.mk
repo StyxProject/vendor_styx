29d8760896a6554499322bf5462ce95948a07ff8
Gagan Malvi <malvigagan@gmail.com>
2024-10-26 22:55:01 +0530

    styx: Use GMS from PixelOS
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -37,9 +37,6 @@ $(call inherit-product, vendor/styx-prebuilts/audio/audio.mk)
 # Inherit bootanimation
 $(call inherit-product, vendor/styx-prebuilts/bootanimation/bootanimation.mk)
 
-# Inherit clocks
-$(call inherit-product, vendor/styx-prebuilts/clocks/product.mk)
-
 # Include common SE policy
 include device/lineage/sepolicy/common/sepolicy.mk
 
@@ -49,27 +46,7 @@ $(call inherit-product-if-exists, vendor/styx-proprietary/products/common.mk)
 # Inherit GMS
 ifneq ($(TARGET_DOES_NOT_USE_GAPPS), true)
 $(warning "Bundling partner GMS.")
-ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.clientidbase=android-google
-else
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
-endif
-
-# Don't dexpreopt prebuilts. (For GMS).
-DONT_DEXPREOPT_PREBUILTS := true
-
-# Include GMS, Modules, and Pixel features.
-$(call inherit-product, vendor/partner_gms/products/gms.mk)
-# $(call inherit-product, vendor/google/pixel/config.mk)
-
-# Anything including updatable_apex.mk should have done so by now.
-ifeq ($(TARGET_FLATTEN_APEX), false)
-$(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules.mk)
-else
-$(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules_flatten_apex.mk)
-endif
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
 endif
 
 # Inherit properties
